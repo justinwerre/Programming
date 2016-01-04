9ff605da3828367f19b86077decb7ee65ee8b9da
justinwerre <werrejustin@gmail.com>
2016-01-03 20:06:57 -0700

    Solved problem a for Rocky mountain conest: popular vote

--- Code/Rocky mountain 2015/a.cpp
+++ Code/Rocky mountain 2015/a.cpp
@@ -1,7 +1,48 @@
+// Problem a: popular vote
+
 #include <iostream>
-using namesapce std;
+using namespace std;
+
+void solve(){
+	int n, max, current, winner, total;
+	bool single_winner = true;
+	total = 0;
+	max = 0;
+	cin >> n;
+
+	for(int i = 1; i <= n; i++){
+		cin >> current;
+
+		total += current;
+		if(current > max){
+			single_winner = true;
+			max = current;
+			winner = i;
+		}else if(current == max){
+			single_winner = false;
+		}
+	}
+
+	if(single_winner){
+		if(max > total / 2){
+			cout << "majority winner " << winner << endl;
+		}else{
+			cout << "minority winner " << winner << endl;
+		}
+	}else{
+		cout << "no winner\n";
+	}
+
+}
 
 int main(){
+	int t;
 
-	retrun 0;
+	cin >> t;
+
+	while(t--){
+		solve();
+	}
+
+	return 0;
 }

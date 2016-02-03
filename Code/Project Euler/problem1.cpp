128a9d4dc2456146fbcca0e7be725e539e6ca98e
justinwerre <werrejustin@gmail.com>
2016-02-02 22:44:31 -0700

    Updated problem 1 to run in constant time.

--- Code/Project Euler/problem1.cpp
+++ Code/Project Euler/problem1.cpp
@@ -3,19 +3,27 @@
 	Multiples of 3 and 5
 	January 27, 2016
 */
-	#include <iostream>
-	using namespace std;
+
+#include <iostream>
+using namespace std;
+
+/*
+	Calculates the sum of all integers between 1 and the target inclusive that are
+	divisible by the divisor. Runs in O(1).
+*/
+int sumDivisibleBy(int divisor, int target){
+	// Rather then iterating over all integers in the range, checking each of them
+	// to see if they are divisible by the divisor, and summing them, we can calculate
+	// how many integers are divisible, find the sum of 1 to that, and then multiply by
+	// the divisor.
+	int p = target / divisor;
+	return divisor * (p * (p + 1) ) / 2;
+}
 
 int main(){
-	unsigned int total = 0;
+	int target = 999;
 
-	for(int i = 3; i < 1000; i++){
-		if(i % 3 == 0 || i % 5 == 0){
-			total += i;
-		}
-	}
-
-	cout << total << endl;
+	cout << sumDivisibleBy(3, target) + sumDivisibleBy(5, target) - sumDivisibleBy(15, target) << endl;
 
 	return 0;
 }
